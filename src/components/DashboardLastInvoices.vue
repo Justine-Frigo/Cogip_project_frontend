a81f6af2f33ebb9811a99174476fd2ede4e026a9
Justine-Frigo <justine0912@gmail.com>
2024-07-18 21:59:56 +0200

    add invoices dashboard

---
+++ src/components/DashboardLastInvoices.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class=" bg-slate-50 p-6 mt-8 rounded-lg shadow h-max">
+        <h3 class="text-lg font-bold mb-6 border-b pb-6">Last invoices</h3>
+        <table class="min-w-full">
+            <thead>
+                <tr class="*:text-left">
+                    <th>
+                        Invoice Number
+                    </th>
+                    <th>
+                        Dates
+                    </th>
+                    <th>
+                        Company
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(invoice, index) in dashboardLastInvoices" :key="index" class="h-12">
+                    <td>
+                        {{ invoice.invoice_number }}
+                    </td>
+                    <td>
+                        {{ invoice.dates_due }}
+
+                    </td>
+                    <td>
+                        {{ invoice.company }}
+
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'DashboardLastInvoices',
+    data() {
+        return {
+            dashboardLastInvoices: null,
+        }
+    },
+    computed: {},
+    async mounted() {
+        this.dashboardLastInvoices = await this.getDashboardLastInvoices()
+    },
+    methods: {
+        async getDashboardLastInvoices() {
+            try {
+                const response = await axios.get("../../mock/invoices.json");
+                return response.data
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,26 +1,27 @@
 <template>
-    <div class="grid grid-cols-2 gap-x-9">
-      <Statistics/>
-    </div>
-  </template>
-  
-  <script>
-  import Statistics from '@/components/Statistics.vue'; 
-    export default {
-      name: 'DashboardView',
-      components:{
-        Statistics,
-      },
-      data() {
-        return {}
-      },
-      computed: {},
-      mounted() {},
-      methods: {}
-    }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style>
-  
-  </style>
+  <div class="grid grid-cols-2 gap-x-9">
+    <Statistics />
+    <DashboardLastInvoices />
+  </div>
+</template>
+
+<script>
+import DashboardLastInvoices from '@/components/DashboardLastInvoices.vue';
+import Statistics from '@/components/Statistics.vue';
+export default {
+  name: 'DashboardView',
+  components: {
+    Statistics,
+    DashboardLastInvoices,
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  mounted() { },
+  methods: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
